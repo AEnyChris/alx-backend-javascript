742aa99e30524f46ea04e2ecf75e6d6d5fe37eea
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-01 11:21:58 +0100

    Added Task 11: iterators

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,9 @@
+export default function createEmployeesObject(departmentName, employees) {
+  const newObject = {
+    [departmentName]: [
+      ...employees,
+    ],
+  };
+
+  return newObject;
+}
