7a24863693afa563d9c5947cd3f41d99abf0e29c
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-03 19:48:26 +0100

    initial commit with README and Task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,15 @@
+/**
+ * Defines a ClassRoom class
+ */
+
+export default class ClassRoom {
+  /**
+   * Implements a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
