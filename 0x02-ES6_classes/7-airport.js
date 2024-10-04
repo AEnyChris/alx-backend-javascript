4ed45af68b702997fd7853d8632f19486fd18f2f
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-04 09:57:53 +0100

    added task 7: customizing default string repr

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,14 @@
+/**
+ * Implement the class Airport
+ */
+
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
