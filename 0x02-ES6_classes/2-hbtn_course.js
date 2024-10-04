5f2f3f041d6647217e7a629ae55e5709f4504182
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-04 06:02:05 +0100

    Added Task 2: setter and getter in classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,47 @@
+/**
+ *Define a HolbertonCourse class
+ */
+
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  set name(n) {
+    if (typeof n !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = n;
+  }
+
+  set length(l) {
+    if (typeof l !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = l;
+  }
+
+  set students(s) {
+    if (!Array.isArray(s)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (!s.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+	}
+    this._students = s;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
