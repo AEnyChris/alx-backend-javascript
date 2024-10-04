667c4610499697a312ec929a7e678ed477a3ccf2
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-04 08:12:26 +0100

    Task 5: Abstract class implementation

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,24 @@
+/**
+ * Implements an abstract class named Building
+ * Instances must override the method evacuationWarningMessage
+ */
+
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = sqft;
+    if (this.constructor !== Building) {
+      if (this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
