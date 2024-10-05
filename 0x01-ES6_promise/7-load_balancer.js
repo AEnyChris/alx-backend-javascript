079b69d4dec9086ffa9dca948e27239de97bd352
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-05 14:54:34 +0100

    added task 7 first resolved promise, the any method

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,10 @@
+/**
+ * A function that takes two promises
+ * and returns the first resolved
+ */
+
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise
+    .any([chinaDownload, USDownload])
+    .then((value) => console.log(value));
+}
