479f630c4d44aaad2924f5df56c4f861871ba34b
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-05 13:34:09 +0100

    correction

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -8,7 +8,7 @@ export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
     .then((res) => {
-      console.log(`${res.body} ${res.firstName} ${res.lastName}`);
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
     .catch(() => { console.log('Signup system offline'); });
 }
