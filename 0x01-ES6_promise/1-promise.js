dd6b03ffeee056d8faa78ccb1028806e4fd2431d
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-05 04:02:22 +0100

    Added Task 1: resolve to JSON

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+/**
+ * A function that returns a Promise
+ */
+
+export default function getResponseFromAPI(success) {
+  return new Promise(((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    }
+    reject(new Error('The fake API is not working currently'));
+  }));
+}
