c617c5f45a05b7deda813ae4ee5dbd2bd12a5198
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-05 14:33:53 +0100

    update to map array to expected output

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -9,5 +9,12 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignUp(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => console.log(values));
+    .then(
+      (values) => values.map((obj) => (
+        {
+          status: obj.status,
+          value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
+        }
+      )),
+    );
 }
