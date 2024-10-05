ac18c5764272927a7742000cc0edd17d95ee9899
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-06 00:14:35 +0100

    added task 8: error catching

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,10 @@
+/**
+ * A Simple function
+ */
+
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
