82384c9b44365d28390a7ef2bd62f04abb08db6c
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-05 13:23:50 +0100

    correction

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,13 +2,13 @@
  * handle multiple promises
  */
 
-import uploadPhoto from './utils';
-import createUser from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+  return Promise
+    .all([uploadPhoto(), createUser()])
     .then((res) => {
-      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+      console.log(`${res.body} ${res.firstName} ${res.lastName}`);
     })
     .catch(() => { console.log('Signup system offline'); });
 }
