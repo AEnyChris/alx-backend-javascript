9ab2e97004cb710f9d125ea95a7b6d20982f4fc2
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-05 07:15:01 +0100

    added task 3: handle multiple promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+/**
+ * handle multiple promises
+ */
+
+import uploadPhoto from './utils';
+import createUser from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto, createUser])
+    .then((res) => {
+      console.log(`${resi[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => { console.log('Signup system offline'); });
+}
