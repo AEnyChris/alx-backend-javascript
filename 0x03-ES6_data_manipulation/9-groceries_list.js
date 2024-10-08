1c9eaf2516b7d58de319ca61d6dd7d6a206a2ee0
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-08 10:53:59 +0100

    added task 8

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,14 @@
+/**
+ * creates a new Map with given values
+ */
+
+export default function groceriesList() {
+  const grocMap = new Map();
+  grocMap.set('Apples', 10);
+  grocMap.set('Tomatoes', 10);
+  grocMap.set('Pasta', 1);
+  grocMap.set('Rice', 1);
+  grocMap.set('Banana', 5);
+
+  return grocMap;
+}
