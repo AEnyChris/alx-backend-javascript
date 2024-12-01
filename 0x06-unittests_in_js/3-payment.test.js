c81e7500761a0a72ace955bee5121a349cd94398
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-12-01 22:40:16 +0100

    corrections on 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -6,9 +6,10 @@ const Utils = require('./utils');
 describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi should be equal to Utils.calculateNumber', () => {
     const consoleSpy = sinon.spy(console, 'log');
+    const utils = sinon.spy(Utils)
     sendPaymentRequestToApi(100, 20);
     const sumRes = parseInt(consoleSpy.args[0][0].slice(14));
-
+    expect(utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true
     expect(Utils.calculateNumber('SUM', 100, 20)).to.equal(sumRes);
     consoleSpy.restore();
   });
