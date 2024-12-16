386d93dd2e96a9d981391d7f1dafe323a7aaa52b
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-12-16 09:49:03 +0100

    added task 2 with yarnlock file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,18 +1,34 @@
-const fs = require('fs')
+const fs = require('fs');
 
-let countStudents = function (path) {
+const countStudents = (path) => {
+  // Throw error if file does not exist of path is not a file
   try {
-    if ((!fs.existsSync(path)) | !(fs.statSync(path).isFile())) {
+    if ((!fs.existsSync(path)) || !(fs.statSync(path).isFile())) {
       throw new Error('Cannot load the database');
     }
-  } catch (error){
+  } catch (error) {
     throw new Error('Cannot load the database');
   }
 
-  const fileLines = fs.readFileSync(path).toString('utf-8').split('\n')
-  console.log(`Number of students: ${fileLines.length - 1}`)
-  console.log(fileLines)
+  // Read file and process each line to save count of each field
+  // and corresponding first name of associated student into an object
+  const fileLines = fs.readFileSync(path).toString('utf-8').split('\n');
+  const fieldsCount = {};
+  for (const i of fileLines.slice(1)) {
+    if (!Object.keys(fieldsCount).includes(i.split(',').at(-1))) {
+      fieldsCount[i.split(',').at(-1)] = {};
+      fieldsCount[i.split(',').at(-1)][i.split(',').at(-1)] = 1;
+      fieldsCount[i.split(',').at(-1)].firstNames = new Array(i.split(',').at(0));
+    } else {
+      fieldsCount[i.split(',').at(-1)][i.split(',').at(-1)] += 1;
+      fieldsCount[i.split(',').at(-1)].firstNames.push(i.split(',').at(0));
+    }
+    // console.log(i.split(',').at(-1))
+  }
+  console.log(`Number of students: ${fileLines.length - 1}`);
+  Object.values(fieldsCount).forEach((field) => console.log(`Number of students in ${Object.keys(field)[0]}: ${field[Object.keys(field)[0]]}. List: ${field.firstNames.toString().replace(/,/g, ', ')}`));
+  // console.log(Object.keys(fieldsCount));
+  // console.log(fileLines);
 };
 
-
-countStudents('./database.csv');
+module.exports = countStudents;
