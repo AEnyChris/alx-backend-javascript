0f14ba547d205bf92ad455279606d9183007e09a
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-12-16 23:27:27 +0100

    provision for empty line at end of database

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,43 +2,29 @@ const { readFile } = require('node:fs/promises');
 const { resolve } = require('node:path');
 
 async function countStudents(path) {
-  // try {
-  //   await access(path)
-  // } catch (err) {
-  //   throw new Error('Cannot load the database')
-  // }
-
   try {
     const filePath = resolve(path);
     const contents = await readFile(filePath, { encoding: 'utf8' });
-    const fileLines = contents.split('\n');
-    // console.log(contents);
-    // console.log(fileLines)
-    // console.log(typeof(contents));
+    const fileLines = contents.trim().split('\n');
     const fieldsCount = {};
+
     for (const i of fileLines.slice(1)) {
       if (!Object.keys(fieldsCount).includes(i.split(',').at(-1))) {
         fieldsCount[i.split(',').at(-1)] = {};
-        fieldsCount[i.split(',').at(-1)][i.split(',').at(-1)] = 1;
+        fieldsCount[i.split(',').at(-1)].count = 1;
         fieldsCount[i.split(',').at(-1)].firstNames = new Array(i.split(',').at(0));
       } else {
-        fieldsCount[i.split(',').at(-1)][i.split(',').at(-1)] += 1;
+        fieldsCount[i.split(',').at(-1)].count += 1;
         fieldsCount[i.split(',').at(-1)].firstNames.push(i.split(',').at(0));
       }
     }
+
     console.log(`Number of students: ${fileLines.length - 1}`);
-    Object.values(fieldsCount).forEach((field) => console.log(`Number of students in ${Object.keys(field)[0]}: ${field[Object.keys(field)[0]]}. List: ${field.firstNames.toString().replace(/,/g, ', ')}`));
+    Object.entries(fieldsCount).forEach((ele) => console.log(`Number of students in ${ele[0]}: ${ele[1].count}. List: ${ele[1].firstNames.toString().replace(/,/g, ', ')}`))
   } catch (err) {
     throw new Error('Cannot load the database');
-    // console.error(err.message);
   }
 }
 
 module.exports = countStudents;
-// countStudents('databas.csv')
 
-// (async () => {try {
-//   await countStudents('database.csv');
-// } catch (err) {
-//   console.error(err)
-// }})();
