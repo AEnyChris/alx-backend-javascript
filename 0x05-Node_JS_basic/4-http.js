13e27814845b3b59f49c175f431c1da7a51f6e1d
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-12-17 02:12:38 +0100

    correction

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,4 +1,4 @@
-const { createServer } = require('node:http');
+const { createServer } = require('http');
 
 const hostname = '127.0.0.1';
 const port = 1245;
@@ -6,10 +6,11 @@ const port = 1245;
 const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello ALX!');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+module.exports = app
