eba5fe234a19f8493eaf1835a92be486a9348b18
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-12-17 02:00:42 +0100

    added task 4 and renamed the babel file to js extension

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,15 @@
 const { createServer } = require('node:http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 1245;
 
-const server = createServer((req, res) => {
+const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello ALX!');
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
