68c23f4835c78100257f06f1182eeab63c3533bc
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-12-17 01:34:14 +0100

    modified task 3 to return a Promise instead of async/await

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,29 +1,33 @@
-const { readFile } = require('node:fs/promises');
-const { resolve } = require('node:path');
+const fs = require('fs');
 
-async function countStudents(path) {
-  try {
-    const filePath = resolve(path);
-    const contents = await readFile(filePath, { encoding: 'utf8' });
-    const fileLines = contents.trim().split('\n');
-    const fieldsCount = {};
-
-    for (const i of fileLines.slice(1)) {
-      if (!Object.keys(fieldsCount).includes(i.split(',').at(-1))) {
-        fieldsCount[i.split(',').at(-1)] = {};
-        fieldsCount[i.split(',').at(-1)].count = 1;
-        fieldsCount[i.split(',').at(-1)].firstNames = new Array(i.split(',').at(0));
-      } else {
-        fieldsCount[i.split(',').at(-1)].count += 1;
-        fieldsCount[i.split(',').at(-1)].firstNames.push(i.split(',').at(0));
-      }
+const countStudents = (path) => new Promise((res, rej) => {
+  fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      rej(new Error('Cannot load the database'));
     }
 
-    console.log(`Number of students: ${fileLines.length - 1}`);
-    Object.entries(fieldsCount).forEach((ele) => console.log(`Number of students in ${ele[0]}: ${ele[1].count}. List: ${ele[1].firstNames.toString().replace(/,/g, ', ')}`));
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
-}
+    if (data) {
+      const fileLines = data.trim().split('\n');
+      const fieldsCount = {};
+
+      for (const i of fileLines.slice(1)) {
+        if (!Object.keys(fieldsCount).includes(i.split(',').at(-1))) {
+          fieldsCount[i.split(',').at(-1)] = {};
+          fieldsCount[i.split(',').at(-1)].count = 1;
+          fieldsCount[i.split(',').at(-1)].firstNames = new Array(i.split(',').at(0));
+        } else {
+          fieldsCount[i.split(',').at(-1)].count += 1;
+          fieldsCount[i.split(',').at(-1)].firstNames.push(i.split(',').at(0));
+        }
+      }
+
+      console.log(`Number of students: ${fileLines.length - 1}`);
+      Object.entries(fieldsCount).forEach((ele) => console.log(`Number of students in ${ele[0]}: ${ele[1].count}. List: ${ele[1].firstNames.toString().replace(/,/g, ', ')}`));
+      res(1);
+    }
+  });
+});
 
 module.exports = countStudents;
+
+// countStudents('database.csv')
