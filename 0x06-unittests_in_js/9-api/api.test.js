8668aa114831cdab76b3e5761c0679868dbff095
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-12-21 13:49:49 +0100

    lint correction

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,36 +1,35 @@
-const chai = require('chai')
-const expect = chai.expect
+const chai = require('chai');
+
+const { expect } = chai;
 const request = require('request');
 
-describe('Testing index page', () =>{
-  it('returns the correct response', (done) => {
+describe('testing index page', () => {
+  it('returns the correct response', () => new Promise((done) => {
     request('http://localhost:7865 ', (error, response, body) => {
       expect(error).to.be.equal(null);
       expect(body).to.be.equal('Welcome to the payment system');
-      expect(response.statusCode).to.be.equal(200)
+      expect(response.statusCode).to.be.equal(200);
       done();
-    })
-  })
-}
-)
+    });
+  }));
+});
 
-describe('Testing cart page', () =>{
-  it('returns the correct response for a number id', (done) => {
-    const id = 23
+describe('testing cart page', () => {
+  it('returns the correct response for a number id', () => new Promise((done) => {
+    const id = 23;
     request(`http://localhost:7865/cart/${id}`, (error, response, body) => {
       expect(error).to.be.equal(null);
       expect(body).to.be.equal(`Payment method for cart ${id}`);
-      expect(response.statusCode).to.be.equal(200)
+      expect(response.statusCode).to.be.equal(200);
       done();
-    })
-  })
+    });
+  }));
 
-  it('returns 404 for id not a numnber', (done) => {
-    const id = 'hello'
+  it('returns 404 for id not a numnber', () => new Promise((done) => {
+    const id = 'hello';
     request(`http://localhost:7865/cart/${id}`, (error, response, body) => {
-      expect(response.statusCode).to.be.equal(404)
+      expect(response.statusCode).to.be.equal(404);
       done();
-    })
-  })
-}
-)
+    });
+  }));
+});
